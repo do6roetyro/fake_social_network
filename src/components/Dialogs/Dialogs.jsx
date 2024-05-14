bf6b157aa70a55c6ccb5526036fa87a42752e40c
dobroeytro <warm.citrus@gmail.com>
2024-05-14 21:59:53 +0300

    вёрстка компоненты dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,9 +4,19 @@ import style from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
-   
-      <div className="dialog">
-       dialog
+
+    <div className={style.dialogs}>
+      <ul className={style.dialogs_list}>
+        <li className={`${style.dialog_item} ${style.active}`}>Alex</li>
+        <li className={style.dialog_item}>Boris</li>
+        <li className={style.dialog_item}>Helena</li>
+        <li className={style.dialog_item}>John</li>
+      </ul>
+      <ul className={style.messages_list}>
+        <li className={style.message}>Hello</li>
+        <li className={style.message}>Hello, how are you?</li>
+        <li className={style.message}>I'm fine, and you?</li>
+      </ul>
     </div>
   )
 }
